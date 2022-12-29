1d12f199f4ae589b64e0159c7cb782b66d56328b
kamron-dev <k.kamron@yadnex.ru>
2022-12-29 13:10:14 +0500

    tweaking the plus button, making the calc work

--- app.js
+++ app.js
@@ -4,12 +4,6 @@ const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2; 
 const divide = (num1, num2) => num1 / num2;
 
-
-// Initialzing buttons and display elements
-const buttonsContainer = document.querySelector('.buttonsEl')
-const displayEl = document.querySelector('.display')
-const clearBtn = document.getElementById('aC')
-
 // Creating an operate func to call above operators on numbers 
 const operate = (operator, num1, num2) => {
     if (operator === add) return add(num1, num2);
@@ -17,6 +11,21 @@ const operate = (operator, num1, num2) => {
     if (operator === multiply) return multiply(num1, num2);
     if (operator === divide) return divide(num1, num2)
 }
+
+
+// Initialzing buttons and display element
+const buttonsContainer = document.querySelector('.buttonsEl');
+const displayEl = document.querySelector('.display');
+const clearBtn = document.getElementById('aC');
+
+const plusBtn = document.getElementById('plusBtn');
+const minusBtn = document.getElementById('minusBtn');
+const mulptiplyBtn = document.getElementById('multiplyBtn')
+const divideBtn = document.getElementById('divideBtn')
+const equalsBtn = document.getElementById('equalsBtn')
+
+
+
 
 displayEl.textContent = ''
 
@@ -31,15 +40,27 @@ function getNumberButtons() {
     return allButtons
 }
 
+
+
+
 allNumberButtons.forEach(button => {
     button.addEventListener('click', () => {
         displayEl.textContent += button.textContent
     })
 });
 
+plusBtn.addEventListener('click', () => {
+    num1 = displayEl.textContent;
+    displayEl.textContent = '';
+    
+})
+
+
+
+
 clearBtn.addEventListener('click', () => {
     displayEl.textContent = ''
-})
+});
 
 // button8.addEventListener('click', () => {
 //     displayEl.textContent = button8.textContent
