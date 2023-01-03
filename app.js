706e094da6458d5f1b4386858bb5864dbbab40e9
kamron-dev <k.kamron@yadnex.ru>
2023-01-03 15:20:48 +0500

    calc working

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+let lastOperand = null;
+let newOperand = null;
+let result; 
+let operator;
+
+
 // Creatring operator functions
 const add = (num1, num2) => num1 + num2; 
 const subtract = (num1, num2) => num1 - num2;
@@ -6,16 +12,17 @@ const divide = (num1, num2) => num1 / num2;
 
 // Creating an operate func to call above operators on numbers 
 const operate = (operator, num1, num2) => {
-    if (operator === add) return add(num1, num2);
-    if (operator === subtract) return subtract(num1, num2);
-    if (operator === multiply) return multiply(num1, num2);
-    if (operator === divide) return divide(num1, num2)
+    if (operator === '+') return add(num1, num2);
+    if (operator === '-') return subtract(num1, num2);
+    if (operator === 'x') return multiply(num1, num2);
+    if (operator === '÷') return divide(num1, num2)
 }
 
 
 // Initialzing buttons and display element
 const buttonsContainer = document.querySelector('.buttonsEl');
-const displayEl = document.querySelector('.display');
+let display1El = document.querySelector('.display');
+let display2El = document.querySelector('.display2');
 const clearBtn = document.getElementById('aC');
 
 const plusBtn = document.getElementById('plusBtn');
@@ -27,41 +34,81 @@ const equalsBtn = document.getElementById('equalsBtn')
 
 
 
-displayEl.textContent = ''
+display2El.textContent = ''
+display1El.textContent = ''
 
 
-const allNumberButtons = buttonsContainer.querySelectorAll(getNumberButtons())
+const allNumberButtons = buttonsContainer.querySelectorAll(".operand")
+const allOperatorBtns = buttonsContainer.querySelectorAll(".operator")
 
-function getNumberButtons() {
-    let allButtons = [];
-    for (let i = 0; i <= 9; i++) {
-        allButtons.push(`#b${i}`) 
-    }
-    return allButtons
+allOperatorBtns.forEach(button => {
+    button.addEventListener('click', () => {
+        if (display1El.textContent === '') {
+          lastOperand = display2El.textContent;
+          display1El.textContent = display2El.textContent;
+          display1El.textContent += button.value;
+          display2El.textContent = '';
+          operator = button.value;
+
+        } else {
+            display1El.textContent += button.value;
+            operator = button.value;
+            newOperand = display2El.textContent;
+        }
+        
+        
+        
+        
+        // displayEl.textContent += operator
+        // clearScreen()
+
+    })
+})
+
+equalsBtn.addEventListener('click', () => {
+    newOperand = display2El.textContent;
+    showResult()
+    
+    
+})
+
+// function clearScreen() {
+//     displayEl.textContent = ''
+// }
+
+function showResult() {
+    result = operate(operator, Number(lastOperand), Number(newOperand));
+    display1El.textContent = result
+    display2El.textContent = '';
+    operator = null;
+    lastOperand = result
 }
-
-
 
 
 allNumberButtons.forEach(button => {
     button.addEventListener('click', () => {
-        displayEl.textContent += button.textContent
+       display2El.textContent += button.value
+       
     })
 });
 
-plusBtn.addEventListener('click', () => {
-    num1 = displayEl.textContent;
-    displayEl.textContent = '';
-    
-})
+
+
+// plusBtn.addEventListener('click', () => {
+//     display1El.textContent = display2El.textContent
+//     display2El.textContent = ''
+//     num1 = displayEl.textContent;
+//     num2 = displayEl.textContent;
+//     operate(add, num1, num2)
+
+// })
 
 
 
 
 clearBtn.addEventListener('click', () => {
-    displayEl.textContent = ''
+    display1El.textContent = ''
+    display2El.textContent = ''
 });
 
-// button8.addEventListener('click', () => {
-//     displayEl.textContent = button8.textContent
-// })
+
